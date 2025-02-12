b2b46591014ca0cd72275563217f63fafa00219b
Chaitanya-Pratap-Singh <chaitanyapratapsingh24@gmail.com>
2025-02-13 00:54:41 +0530

    intial commits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import {AnimatedTestimonialsDemo} from "@/app/components/animated-testimonials-demo";
-import FlipCard from "@/app/components/FlipCards";
-import AboutPageFlipCards from "@/app/components/AboutPageFlipCards";
+/** @format */
 
+import { AnimatedTestimonialsDemo } from "@/app/components/animated-testimonials-demo";
+import FlipCard from "@/app/components/FlipCards";
 
 export default function Home() {
-  return (
-    <>
-      <AnimatedTestimonialsDemo/>
-      <FlipCard/>
-      <AboutPageFlipCards/>
-    </>
-  );
+	return (
+		<>
+			<AnimatedTestimonialsDemo />
+			<FlipCard />
+		</>
+	);
 }

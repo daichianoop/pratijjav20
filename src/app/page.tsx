1d1eb174c8a39c1ead7c012f4cde28acef8ec1bb
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-02-14 00:54:39 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import {AnimatedTestimonialsDemo} from "@/app/components/animated-testimonials-demo";
 import FlipCard from "@/app/components/FlipCards";
 import AboutPageFlipCards from "@/app/components/AboutPageFlipCards";
+import Footer from "@/app/components/Footer";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <AnimatedTestimonialsDemo/>
       <FlipCard/>
       <AboutPageFlipCards/>
+        <Footer/>
     </>
   );
 }

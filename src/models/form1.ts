44619bb5c48eb3d4b6e2ca6ed0fa5ae8c93bbb11
Chaitanya-Pratap-Singh <chaitanyapratapsingh24@gmail.com>
2025-02-20 12:37:08 +0530

    schema updated

--- src/models/form1.ts
+++ src/models/form1.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import mongoose, { Schema, Document, model } from "mongoose";
+import{ Schema, Document, model } from "mongoose";
 
 // Define an interface for the cross-team form data
 interface Form1 extends Document {

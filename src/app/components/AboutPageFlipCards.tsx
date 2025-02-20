a2f3365794bd931954a09d7393574a78e2407feb
Chaitanya-Pratap-Singh <chaitanyapratapsingh24@gmail.com>
2025-02-21 00:19:23 +0530

    about page final

--- src/app/components/AboutPageFlipCards.tsx
+++ src/app/components/AboutPageFlipCards.tsx
@@ -1,69 +1,62 @@
-import React from 'react';
-import './styles/flipCards.css';
-import Image from "next/image";
-import { Yeseva_One } from 'next/font/google';
+/** @format */
 
+import React from "react";
+import "./styles/flipCards.css";
+import Image from "next/image";
+import { Yeseva_One } from "next/font/google";
 
 const yeseva = Yeseva_One({
-    weight: ['400'],
-    subsets: ['latin'],
+	weight: ["400"],
+	subsets: ["latin"],
 });
 
-
 const FlipCard: React.FC = () => {
-    return (
-        <>
-            <section className="flipCards px-20 py-10" style={{ backgroundColor: "#EBCE89" }}>
-                <div className="flex flex-row justify-around py-4">
-                    <div className="flip-card">
-                        <div className="flip-card-inner">
-                            <div className="flip-card-front">
-                                <Image src={"/poster-21.png"} width={390} height={500} alt={"21"}/>
-                            </div>
-                            <div className="px-7 py-9 flip-card-back bg-[url('/flipcardbg.png')] bg-cover bg-center h-screen">
-                                <p className={`title mb-5 text-4xl ${yeseva.className}`}>INCEPTION</p>
-                                <p className={`h-full text-lg ${yeseva.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                    in reprehenderit in voluptate velit esse </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flip-card -translate-y-20">
-                        <div className="flip-card-inner">
-                            <div className="flip-card-front">
-                                <Image src={"/poster-22.png"} width={390} height={500} alt={"21"}/>
-                            </div>
-                            <div className="px-7 py-9 flip-card-back bg-[url('/flipcardbg.png')] bg-cover bg-center h-screen">
-                                <p className={`title mb-5 text-4xl ${yeseva.className}`}>DISCLOSURE</p>
-                                <p className={`h-full text-lg ${yeseva.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                    in reprehenderit in voluptate velit esse </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flip-card">
-                        <div className="flip-card-inner">
-                            <div className="flip-card-front">
-                                <Image src={"/poster-23.png"} width={390} height={500} alt={"21"}/>
-                            </div>
-                            <div className="px-7 py-9 flip-card-back bg-[url('/flipcardbg.png')] bg-cover bg-center h-screen">
-                                <p className={`title mb-5 text-4xl ${yeseva.className}`}>EPILOGUE</p>
-                                <p className={`h-full text-lg ${yeseva.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                    in reprehenderit in voluptate velit esse </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
-}
+	return (
+		<section
+			className="cards-container px-4 sm:px-10 py-10"
+			style={{ backgroundColor: "#EBCE89" }}>
+			<div className="flex flex-col md:flex-row justify-center gap-8 items-start">
+				{/* Card 1 */}
+				<div className="custom-card w-full sm:w-[300px] h-[400px] relative">
+					<div className="card-content p-4">
+						<Image
+							src="/poster-kiit.png"
+							width={300}
+							height={300}
+							alt="KIIT & KISS Poster"
+							className="card-image object-cover rounded-md"
+						/>
+					</div>
+				</div>
+
+				{/* Card 2 */}
+				<div className="custom-card w-full sm:w-[300px] h-[400px] relative">
+					<div className="card-content p-4">
+						<Image
+							src="/poster-pratijja.png"
+							width={300}
+							height={300}
+							alt="Pratijja Poster"
+							className="card-image object-cover rounded-md"
+						/>
+					</div>
+				</div>  
+
+				{/* Card 3 */}
+				<div className="custom-card w-full sm:w-[300px] h-[400px] relative">
+					<div className="card-content p-4">
+						<Image
+							src="/poster-founder.png"
+							width={300}
+							height={300}
+							alt="Founder Poster"
+							className="card-image object-cover rounded-md"
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 export default FlipCard;
--- src/app/components/AboutUs.tsx
+++ src/app/components/AboutUs.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import Image from "next/image";
 import { Barlow } from "next/font/google";
+import FlipCard from "./AboutPageFlipCards";
+import "../globals.css"; // ensure this path matches your project structure
 
 const barlow = Barlow({
 	weight: "900", // specify desired weight
@@ -10,7 +12,7 @@ const barlow = Barlow({
 
 const AboutUs = () => {
 	return (
-		<div className="min-h-screen">
+		<div className="min-h-screen bg-[#002027]">
 			{/* Hero Section with Gradient Overlay */}
 			<div className="bg-[#002027]">
 				<section className="relative bg-[#002027]">
@@ -19,25 +21,26 @@ const AboutUs = () => {
 						className="absolute inset-0"
 						style={{ backgroundColor: "#EBCE89" }}></div>
 
-					<div className="container mx-auto py-16 px-4 relative text-center">
+					<div className="relative container mx-auto py-16 px-4 text-center">
 						<h1
-							className="text-[120px] font-bold text-teal-950 leading-none text-center"
+							className="text-[10vw] font-bold text-teal-950 leading-none text-center"
 							style={{ fontFamily: "Impact, sans-serif" }}>
 							ABOUT US
 						</h1>
-						<p className="text-lg text-teal-950 max-w-3xl mt-4 mx-auto">
+						<p className=" text-base sm:text-lg max-w-3xl mt-4 mx-auto text-teal-950">
 							KRONICLE, KIIT&apos;s official debating society, brings its
 							mission to reality once again, with its own APD competition—a
 							monument to our founder&apos;s intellect, argumentation, and
 							unwavering pursuit of discourse.
 						</p>
 					</div>
+					<FlipCard />
 				</section>
 
 				{/* Version Section with Background Text */}
 				<section className="relative container mx-auto px-4 py-16">
 					<h2
-						className="text-6xl font-bold bg-gradient-to-r from-[#7F654A] via-[#EBCE89] to-[#7F654A] bg-clip-text text-transparent"
+						className="text-3xl sm:text-6xl font-bold bg-gradient-to-r from-[#7F654A] via-[#EBCE89] to-[#7F654A] bg-clip-text text-transparent"
 						style={{ fontFamily: "Impact, sans-serif" }}>
 						PRATIJJA V20.0
 					</h2>
@@ -48,24 +51,23 @@ const AboutUs = () => {
 							<Image
 								src="https://ik.imagekit.io/chaitanya/IMG_3085.png?updatedAt=1740067768461"
 								alt="Debate"
-								className="rounded-lg p-2"
-								style={{ height: "30%" }}
-								width={800}
-								height={400}
+								className="rounded-lg p-2 "
+								width={400}
+								height={300}
 							/>
 						</div>
-						<div className="space-y-6 text-gray-300">
-							<p>
+						<div className="space-y-4 sm:space-y-6 text-gray-300 text-[20px]">
+							<p className="gradient-text">
 								Pratijja is the flagship Asian Parliamentary Debate of KIIT
 								University hosted by its esteemed literary and debating society,
 								Kiitdee.
 							</p>
-							<p>
+							<p className="gradient-text">
 								One of India&apos;s premier debating tournaments, Pratijja
 								attracts debaters from across the country engaging in
 								intellectual discourse and critical thinking.
 							</p>
-							<p>
+							<p className="gradient-text">
 								The tournament follows the Asian Parliamentary format with
 								sessions on various global, societal and cultural issues.
 								Multiple teams participate in intense rounds with
@@ -77,18 +79,17 @@ const AboutUs = () => {
 					{/* "Since 2006" Section with Text Left and Image Right */}
 					<div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center relative mt-16 z-10">
 						<div
-							className="text-[150px] text-amber-100 font-bold leading-none"
+							className="text-[15vw] md:text-[120px] text-amber-100 font-bold leading-none"
 							style={{ fontFamily: "Impact, sans-serif" }}>
 							SINCE
 							<br />
 							2006
 						</div>
-						<div>
+						<div className="flex justify-center">
 							<Image
 								src="https://ik.imagekit.io/chaitanya/IMG_2477.png?updatedAt=1740070800225"
 								alt="Debate"
-								className="rounded-lg mx-auto"
-								style={{ width: "80%" }}
+								className="rounded-lg"
 								width={800}
 								height={600}
 							/>
@@ -109,9 +110,11 @@ const AboutUs = () => {
 									width={40}
 									height={32}
 								/>
-								<h3 className="text-amber-100 text-3xl font-bold">KIIT</h3>
+								<h3 className="text-amber-100 text-xl sm:text-3xl font-bold">
+									KIIT
+								</h3>
 							</div>
-							<p className="text-gray-300 mb-8 leading-relaxed text-[18px]">
+							<p className="gradient-text text-gray-300 mb-8 leading-relaxed text-base sm:text-[18px]">
 								Kalinga Institute of Industrial Technology (KIIT) Deemed to be
 								University has emerged as a leading institute for professional
 								studies. It is admired all over for the quality of its academic
@@ -124,7 +127,7 @@ const AboutUs = () => {
 								founder of KIIT and KISS with an initial investment of Rs. 5000
 								and operating from just two rented rooms, 12 students and 2
 								faculty members as an Industrial Training Institute, KIIT gained
-								acknowledgement in 1997 as a University for undergraduate and
+								acknowledgement in 1997 as an University for undergraduate and
 								postgraduate courses in Engineering, Management and Computer
 								Applications were added. In 2007, many new schools were added to
 								its umbrella – School of Law, Biotechnology, Medical Sciences,
@@ -146,7 +149,7 @@ const AboutUs = () => {
 								auditoriums – the largest one with 5000 seating capacity, 18
 								sports complexes, many international standard stadiums covering
 								all sports, 30 food courts, and a rose garden. The institution
-								takes pride in being the greenest campus in India. It has
+								takes pride in being the greenest campus in India.  It has
 								received all accreditations and affiliations from prestigious
 								national and international bodies such as the Accreditation
 								Board for Engineering and Technology (ABET), USA and the
@@ -155,18 +158,17 @@ const AboutUs = () => {
 								citations. KIIT has had an excellent placement record since its
 								inception owing to its quality teaching and learning process,
 								and the brand value that it carries. KIIT is ranked in the
-								cohort of 601-800 in the Times Higher Education &apos;World
-								University Ranking 2024&apos;. Its Computer Science Engineering
-								ranks in the cohort of 301-400 and its overall Engineering
-								ranking is 401-500 in the world. KIIT has been accredited with
-								an A++ grade by the National Assessment and Accreditation
-								Council (NAAC) with an all-India 15th rank by the National
-								Institution of Ranking Framework, NIRF, Ministry of Education.
-								It has also been the top university for innovation among private
-								institutions for two consecutive years (2020, 2021) according to
-								AICTE, Government of India. It has been ranked 168 among
-								universities in the world as per the Times Higher Education
-								Young University Ranking 2024.
+								cohort of 601-800 in the Times Higher Education ‘World
+								University Ranking 2024’. Its Computer Science Engineering ranks
+								in the cohort of 301-400 and its overall Engineering ranking is
+								401-500 in the world. KIIT has been accredited with an A++ grade
+								by the National Assessment and Accreditation Council (NAAC) with
+								an all-India 15th rank by the National Institution of Ranking
+								Framework, NIRF, Ministry of Education. It has also been the top
+								university for innovation among private institutions for two
+								consecutive years (2020, 2021) according to AICTE, Government of
+								India. It has been ranked 168 among universities in the world as
+								per the Times Higher Education Young University Ranking 2024.
 							</p>
 						</div>
 
@@ -180,12 +182,14 @@ const AboutUs = () => {
 									width={32}
 									height={32}
 								/>
-								<h3 className="text-amber-100 text-3xl font-bold">KISS</h3>
+								<h3 className="text-amber-100 text-xl sm:text-3xl font-bold">
+									KISS
+								</h3>
 							</div>
-							<p className="text-gray-300 mb-8 leading-relaxed text-[20px]">
-								The Kalinga Institute of Social Sciences (KISS), established in
+							<p className="gradient-text text-gray-300 mb-8 leading-relaxed text-base sm:text-[18px]">
+								The Kalinga Institute of Social Sciences(KISS), established in
 								1992 by Dr. Achyuta Samanta, offers free education from
-								kindergarten through post- graduation to children coming from
+								kindergarten through post-graduation to children coming from
 								tribal backgrounds. This institution serves as a medium for
 								providing a holistic educational environment where modernity
 								means indigenousness. Due to this balanced approach, the
@@ -197,30 +201,37 @@ const AboutUs = () => {
 								achieving, thereby, making these children ready to face the
 								world. The institution acts as a medium of uplifting the
 								sidelined sections of Odisha through education. With individuals
-								stepping into public sectors, the alumni network of the
-								institution grows stronger day by day. This alumni connection
+								stepping  into public sectors, the alumni network of the
+								institution grows stronger day by day . This alumni connection
 								actively helps KISS to expand their reach and touch more lives
 								through education. With continued efforts, KISS remains a
 								testament to the success of institutional support in breaking
 								intergenerational cycles of poverty and preserving cultural
-								heritage.
+								heritage. The institute, following its vision-"A world where all
+								children and youth can learn free of cost to learn, lead and
+								grow without fear to reach their full potential"-remains
+								testimony to the possibility of comprehensive educational
+								initiatives in achieving sustainable social development among
+								tribal communities.
 							</p>
+
 							<div className="flex flex-col gap-8">
 								{/* First Row: Image on Left, "KISS" on Right */}
 								<div className="flex flex-col md:flex-row items-center gap-4">
-									<Image
-										src="https://ik.imagekit.io/chaitanya/Untitled%20designfff.png"
-										alt="KISS Campus Image"
-										className="md:w-1/2 rounded-lg"
-										style={{ width: "226px", height: "223px" }}
-										width={226}
-										height={223}
-									/>
+									<div className="md:w-1/2 flex justify-center">
+										<Image
+											src="https://ik.imagekit.io/chaitanya/Untitled%20designfff.png"
+											alt="KISS Campus Image"
+											className="rounded-lg"
+											width={226}
+											height={223}
+										/>
+									</div>
 									<div className="text-center md:text-left w-full md:w-1/2">
 										<div className="mb-2">
 											<div className="h-1 bg-[#EBCE89] w-full mb-2"></div>
 											<p
-												className={`text-[100px] font-bold ${barlow.className}`}
+												className={`text-[10vw] md:text-[100px] font-bold ${barlow.className}`}
 												style={{
 													WebkitTextStroke: "1px #EBCE89",
 													color: "transparent",
@@ -238,7 +249,7 @@ const AboutUs = () => {
 										<div className="mb-2">
 											<div className="h-1 bg-[#EBCE89] w-full mb-2"></div>
 											<p
-												className={`text-[100px] font-bold ${barlow.className}`}
+												className={`text-[10vw] md:text-[100px] font-bold ${barlow.className}`}
 												style={{
 													WebkitTextStroke: "1px #EBCE89",
 													color: "transparent",
@@ -248,14 +259,15 @@ const AboutUs = () => {
 											<div className="h-1 bg-[#EBCE89] w-full mt-2"></div>
 										</div>
 									</div>
-									<Image
-										src="https://ik.imagekit.io/chaitanya/ups.png"
-										alt="KIIT Campus Image"
-										className="md:w-1/2 rounded-lg"
-										style={{ width: "294px", height: "263px" }}
-										width={294}
-										height={263}
-									/>
+									<div className="md:w-1/2 flex justify-center">
+										<Image
+											src="https://ik.imagekit.io/chaitanya/ups.png"
+											alt="KIIT Campus Image"
+											className="rounded-lg"
+											width={294}
+											height={263}
+										/>
+									</div>
 								</div>
 							</div>
 						</div>
@@ -265,32 +277,33 @@ const AboutUs = () => {
 				{/* Founder Section */}
 				<section className="container mx-auto px-4 py-16">
 					<h2
-						className="text-[50px] text-center w-1/2 mx-auto font-bold text-amber-100 mb-8"
+						className="text-[8vw] md:text-[50px] text-center w-full md:w-1/2 mx-auto font-bold mb-8 bg-gradient-to-r from-[#7F654A] via-[#EBCE89] to-[#7F654A] bg-clip-text text-transparent"
 						style={{ fontFamily: "Impact, sans-serif" }}>
 						ABOUT FOUNDER OF KIIT AND KISS
 					</h2>
 
 					<div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-						<Image
-							src="https://ik.imagekit.io/chaitanya/Achyuta-Samanta-Founder-of-KIIT-KISS.png?updatedAt=1740037879854"
-							alt="Founder"
-							className="rounded-lg mx-auto"
-							style={{ width: "313px", height: "400px" }}
-							width={313}
-							height={400}
-						/>
-						<div className="text-gray-300 space-y-4 leading-relaxed text-[20px]">
-							<p>
+						<div className="flex justify-center">
+							<Image
+								src="https://ik.imagekit.io/chaitanya/Achyuta-Samanta-Founder-of-KIIT-KISS.png?updatedAt=1740037879854"
+								alt="Founder"
+								className="rounded-lg"
+								width={313}
+								height={400}
+							/>
+						</div>
+						<div className="text-gray-300 space-y-4 leading-relaxed text-base sm:text-[18px]">
+							<p className="gradient-text">
 								Prof. Dr. Achyuta Samanta is an Indian philanthropist who has
 								dedicated his life to ensuring quality education and healthcare
 								for the less privileged communities.
 							</p>
-							<p>
+							<p className="gradient-text">
 								His work and service in the fields of education, social welfare,
 								and women empowerment have shaped his journey and have earned
 								him many national and international awards and honors.
 							</p>
-							<p>
+							<p className="gradient-text">
 								Motivated by his mission—&apos;zero poverty, zero hunger, and
 								zero illiteracy&apos;—he has been selfless in his contributions
 								for the upliftment of the rural and indigenous groups of the

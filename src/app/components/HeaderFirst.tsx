0bea78552ffb6fcb60d8f354ea2722b62efe3666
Chaitanya-Pratap-Singh <chaitanyapratapsingh24@gmail.com>
2025-02-20 12:56:58 +0530

    frontpage updates

--- src/app/components/HeaderFirst.tsx
+++ src/app/components/HeaderFirst.tsx
@@ -1,30 +1,34 @@
+/** @format */
+
 import React from "react";
 import Image from "next/image";
 import "./styles/headerFirstPage.css";
+import Navbar from "./Navbar";
 
 const HeaderFirstPage: React.FC = () => {
-    return (
-        <div className={"fakebody"}>
-            <div className="container">
-                <div className="title">
-                    20<sup>th</sup> Edition Of
-                </div>
-                <div className="one-of">One Of</div>
-                <div className="debate-text">
-                    The Largest Asian Parliamentary Debate <br/> 2025
-                </div>
-                <div className="image">
-                    <Image
-                        src="/image.webp"
-                        alt="Event"
-                        width={500} // Set appropriate width
-                        height={300} // Set appropriate height
-                        priority // Loads the image faster for better performance
-                    />
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={"fakebody"}>
+
+			<div className="container">
+				<div className="title">
+					20<sup>th</sup> Edition Of
+				</div>
+				<div className="one-of">One Of</div>
+				<div className="debate-text">
+					The Largest Asian Parliamentary Debate <br /> 2025
+				</div>
+				<div className="image">
+					<Image
+						src="/image.webp"
+						alt="Event"
+						width={500} // Set appropriate width
+						height={300} // Set appropriate height
+						priority // Loads the image faster for better performance
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HeaderFirstPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import {AnimatedTestimonialsDemo} from "@/app/components/animated-testimonials-demo";
 import FlipCard from "@/app/components/FlipCards";
 import AboutPageFlipCards from "@/app/components/AboutPageFlipCards";
+import Navbar from "./components/Navbar";
+import HeaderFirstPage from "./components/HeaderFirst";
 
 
 
 export default function Home() {
   return (
     <>
-      <AnimatedTestimonialsDemo/>
-        <FlipCard/>
-        <AboutPageFlipCards/>
+    <HeaderFirstPage/>
+      <FlipCard/>
     </>
   );
 }

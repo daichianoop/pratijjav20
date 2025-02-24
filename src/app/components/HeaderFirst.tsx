9d4ff72b7dbc0379d1c7419e8f73ea1a5451f805
Chaitanya-Pratap-Singh <chaitanyapratapsingh24@gmail.com>
2025-02-25 03:21:09 +0530

    Revert "small fix"
    This reverts commit 65217115d7c4529fe787191777616566c42aceb5.

--- src/app/components/HeaderFirst.tsx
+++ src/app/components/HeaderFirst.tsx
@@ -9,28 +9,24 @@ const HeaderFirstPage: React.FC = () => {
 		<div className={"fakebody bg-no-repeat bg-fixed bg-contain"}>
 			<div className="container">
 				<div className="title">
-					20<sup>TH</sup>&nbsp;EDITION OF
+				20<sup>TH</sup>&nbsp;EDITION OF
 				</div>
 				<div className="one-of">One Of</div>
-
+				<div className="debate-text">
+					THE LARGEST ASIAN PARLIAMENTARY DEBATE <br/> 2025
+				</div>
 				<div className="image">
 					<Image
 						src="/image.webp"
 						alt="Event"
 						width={400} // Set appropriate width
 						height={300} // Set appropriate height
-						priority
+						priority 
 					/>
 				</div>
-				<div className="debate-text ">
-					<div>
-						<span className="bigtext">THE LARGEST </span>
-						ASIAN PARLIAMENTARY DEBATE IN INDIA
-					</div>
-				</div>
 			</div>
-			<div className={"relative -right-10 -bottom-20 count"}>
-				<CountdownTimer />
+			<div className={"relative -right-10 -bottom-20"}>
+				<CountdownTimer/>
 			</div>
 		</div>
 	);

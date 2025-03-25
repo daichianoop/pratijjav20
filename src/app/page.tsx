4686b1430cb7a1cdb5b04c348492757109b0d616
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-03-26 00:47:15 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,40 @@
-/** @format */
+"use client";
+import CircularText from "../components/CircularText";
+import { Great_Vibes } from "next/font/google";
+import "../components/styles/bg-change.css"
+import GradientText from "@/components/colorText";
 
-import FlipCard from "@/components/FlipCards";
-import HeaderFirstPage from "@/components/HeaderFirst";
-import { AnimatedTestimonialsDemo } from "@/components/animated-testimonials-demo";
+const handwritingFont = Great_Vibes({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-handwriting",
+});
 
 export default function Home() {
-	return (
-		<>
-			<div className={"font-built-titling"}>
-				<div className="fakebody w-screen h-screen text-[#ebce89] px-1.5">
-					<HeaderFirstPage />
-				</div>
-				<AnimatedTestimonialsDemo />
-				<FlipCard />
-			</div>
-		</>
-	);
+  return (
+    <div className="relative h-screen w-screen  overflow-hidden">
+      <div
+        className="absolute bg-elem inset-0"/>
+      <div className="relative z-10 grid h-full w-full place-content-center">
+        <section>
+          <CircularText
+            text="PRATIJJA*V21*LOADING*"
+            onHover="speedUp"
+            spinDuration={10}
+            className="custom-class"
+          />
+          <div className={`text-5xl mt-10 text-white ${handwritingFont.variable} font-[var(--font-handwriting)]`}>
+            <GradientText
+              colors={["#ebce89", "#fff", "#ebce89", "#fff", "#ebce89"]}
+              animationSpeed={3}
+              showBorder={false}
+              className="custom-class text-center"
+            >
+              Untill Next Year! Pratijja v21 Awaits.
+            </GradientText>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }

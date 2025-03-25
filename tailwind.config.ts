78f2172af90d839c13e4a4e98d4e3354de3fe5fb
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-03-26 00:49:00 +0530

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
       fontFamily: {
         'built-titling': ['"built-titling"', 'sans-serif'], // Register the custom font
       },
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      animation: {
+        gradient: 'gradient 8s linear infinite',
+      },
     },
   },
   plugins: [],
